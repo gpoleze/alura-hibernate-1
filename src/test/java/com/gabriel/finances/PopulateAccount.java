bef3675272ef350291a627b2ebcd09ae1e191a95
Gabriel Ferreira <gabriel@jostle.me>
2018-06-23 12:04:27 -0700

    Changed the way the Database is populated after it is created

--- src/test/java/com/gabriel/finances/PopulateAccount.java
+++ src/test/java/com/gabriel/finances/PopulateAccount.java
@@ -1,19 +1,42 @@
 package com.gabriel.finances;
 
 import com.gabriel.finances.model.Account;
+import com.gabriel.finances.model.Category;
+import com.gabriel.finances.model.Transaction;
+import com.gabriel.finances.model.TransactionType;
 import com.gabriel.finances.util.JPAUtil;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import javax.persistence.EntityManager;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Arrays;
 
-public class PopulateAccount {
+class PopulateAccount {
+
+
+    private EntityManager em;
+
+    @BeforeEach
+    void setUp() {
+        em = new JPAUtil().getEntityManager();
+    }
+
+    @AfterEach
+    void tearDown() {
+        try {
+            em.close();
+        } catch (IllegalStateException ignored) {
+        }
+    }
+
 
     @Test
-    public void populateDatabaseWithAccounts() {
+    void populateDatabaseWithAccounts() {
 
-        EntityManager manager = new JPAUtil().getEntityManager();
-
-        manager.getTransaction().begin();
+        em.getTransaction().begin();
 
         Account account1 = new Account();
         Account account2 = new Account();
@@ -47,15 +70,51 @@ public class PopulateAccount {
         account5.setOwner("Alexandre Duarte");
 
         // persisting as accounts
-        manager.persist(account1);
-        manager.persist(account2);
-        manager.persist(account3);
-        manager.persist(account4);
-        manager.persist(account5);
+        em.persist(account1);
+        em.persist(account2);
+        em.persist(account3);
+        em.persist(account4);
+        em.persist(account5);
 
-        manager.getTransaction().commit();
+        em.getTransaction().commit();
 
-        manager.close();
+    }
+
+    @Test
+    void testTransactionWithCategory() {
+        Category category1 = new Category("Trip");
+        Category category2 = new Category("Business");
+
+        Account account = new Account();
+        account.setId(2);
+
+        Transaction transaction1 = new Transaction();
+        transaction1.setType(TransactionType.OUT);
+        transaction1.setValue(new BigDecimal(400));
+        transaction1.setDescription("Trip to Sao Paulo");
+        transaction1.setDate(LocalDateTime.now());
+        transaction1.setCategory(Arrays.asList(category1, category2));
+
+        transaction1.setAccount(account);
+
+        Transaction transaction2 = new Transaction();
+        transaction2.setType(TransactionType.OUT);
+        transaction2.setValue(new BigDecimal(600));
+        transaction2.setDescription("Trip to RJ");
+        transaction2.setDate(LocalDateTime.now().plusDays(7));
+        transaction2.setCategory(Arrays.asList(category1, category2));
+
+        transaction2.setAccount(account);
+
+        em.getTransaction().begin();
+
+        em.persist(category1);
+        em.persist(category2);
+
+        em.persist(transaction1);
+        em.persist(transaction2);
+
+        em.getTransaction().commit();
 
     }
 }
--- src/test/java/com/gabriel/finances/TestJPARelationship.java
+++ src/test/java/com/gabriel/finances/TestJPARelationship.java
@@ -53,44 +53,6 @@ class TestJPARelationship {
     }
 
     @Test
-    void testTransactionWithCategory() {
-        Category category1 = new Category("Trip");
-        Category category2 = new Category("Business");
-
-        Account account = new Account();
-        account.setId(2);
-
-        Transaction transaction1 = new Transaction();
-        transaction1.setType(TransactionType.OUT);
-        transaction1.setValue(new BigDecimal(400));
-        transaction1.setDescription("Trip to Sao Paulo");
-        transaction1.setDate(LocalDateTime.now());
-        transaction1.setCategory(Arrays.asList(category1, category2));
-
-        transaction1.setAccount(account);
-
-        Transaction transaction2 = new Transaction();
-        transaction2.setType(TransactionType.OUT);
-        transaction2.setValue(new BigDecimal(600));
-        transaction2.setDescription("Trip to RJ");
-        transaction2.setDate(LocalDateTime.now().plusDays(7));
-        transaction2.setCategory(Arrays.asList(category1, category2));
-
-        transaction2.setAccount(account);
-
-        em.getTransaction().begin();
-
-        em.persist(category1);
-        em.persist(category2);
-
-        em.persist(transaction1);
-        em.persist(transaction2);
-
-        em.getTransaction().commit();
-
-    }
-
-    @Test
     void testOneAccountCannotHaveTwoOnwers() {
         Owner owner = new Owner();
         owner.setFirstName("Maria");
